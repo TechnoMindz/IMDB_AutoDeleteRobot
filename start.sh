6335b23a7f47788c252e5ec7a5ea667366b9e09c
Techno Mindz <97782515+TechnoMindz@users.noreply.github.com>
2022-06-26 21:44:22 +0530

    Minor Update

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/TechnoMindz/IMDB_AutoDeleteRobot.git /IMDB_AutoDeleteRobot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /IMDB_AutoDeleteRobot
 fi
-cd /EvaMaria
+cd /IMDB_AutoDeleteRobot
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Techno Mindz..."
 python3 bot.py
